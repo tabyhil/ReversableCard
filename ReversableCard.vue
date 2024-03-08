603b50b614f035b2673a9044b5bc28c2ad126887
tabyhil <125238696+tabyhil@users.noreply.github.com>
2024-03-08 20:29:49 +0800

    Add files via upload

---
+++ ReversableCard.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="card">
+        <div class="front">
+            <div>正面</div>
+        </div>
+        <div class="opposite">
+            <div>背面</div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="css" scoped>
+.card {
+    width: 100px;
+    height: 100px;
+}
+
+.card .front,
+.card .opposite {
+    position: absolute;
+    width: 100px;
+    height: 100px;
+    transition: all 1s ease;
+    backface-visibility: hidden;
+}
+
+.card .front {
+    background-color: yellow;
+}
+
+.card .opposite {
+    background-color: green;
+    transform: rotateY(180deg);
+}
+
+.card:hover .front {
+    transform: rotateY(180deg);
+}
+
+.card:hover .opposite {
+    /* 注意rotate是指定角度 */
+    transform: rotateY(0deg);
+}
+</style>
